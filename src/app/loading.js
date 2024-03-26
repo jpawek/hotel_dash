0e27843d839a95972c3ca8cd69efc06408fccf5a
jpawek <josephkewap@gmail.com>
2024-03-25 19:24:48 -0700

    first commit

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Loading() {
+    return (
+        <div id="globalLoader">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="" />
+            <br></br>
+            <p>NOW LOADING...</p>
+        </div>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,94 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function HomePage() {
+
+    const [roomData, setData] = useState([]);
+
+    async function fetchData() {
+        fetch("./api/data-collection", { cache: 'no-store' })
+        .then((response) => response.json())
+        .then((data) => setData(data))
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    return (
+        <>
+        <div className='dash_container'>
+            {roomData.map(room => {
+                if (room[1].occupied) {
+                    //display time due out if room is checking out today
+                    if (room[1].due_out) {
+                        return (
+                            <div className='occ_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>OCCUPIED</p>
+                                <p className='room_info'>DUE OUT</p>
+                                <p className='room_info'>{room[1].checkout_time}</p>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div className='occ_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>OCCUPIED</p>
+                            </div>
+                        );
+                    }
+                } else if (room[1].service_status != 'IN_SERVICE') {
+                    //specify service type in room card
+                    if (room[1].service_status == 'OUT_OF_SERVICE') {
+                        return (
+                            <div className='service_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>OUT OF SERVICE</p>
+                                <pre>{room[1].service_dates[0]}</pre>
+                                <pre>to</pre>
+                                <pre>{room[1].service_dates[1]}</pre>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div className='service_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>OUT OF ORDER</p>
+                                <pre>{room[1].service_dates[0]}</pre>
+                                <pre>to</pre>
+                                <pre>{room[1].service_dates[1]}</pre>
+                            </div>
+                        );
+                    }
+                } else {
+                    //display housekeeping status if room is not occupied or being serviced
+                    if (room[1].status == 'DIRTY') {
+                        return (
+                            <div className='dirty_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>{room[1].status}</p>
+                            </div>
+                        );
+                    } else if (room[1].status == 'PICKUP') {
+                        return (
+                            <div className='pickup_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>{room[1].status}</p>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div className='clean_room' key={room[0]}>
+                                <p className='room_num'>{room[1].number}</p>
+                                <p className='room_info'>{room[1].status}</p>
+                            </div>
+                        );
+                    }
+                }
+            })}
+        </div>
+        </>
+    )
+}
