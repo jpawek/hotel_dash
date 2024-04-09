9ca46358877770b8c370a0a9bf1327c1c32f8d0e
jpawek <josephkewap@gmail.com>
2024-04-08 19:36:19 -0700

    basic user login support

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <h1>DASHBOARD TEST</h1>
+        {children}
+      </body>
     </html>
   );
 }
